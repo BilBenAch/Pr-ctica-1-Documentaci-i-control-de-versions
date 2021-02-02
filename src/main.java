5364b74af00bf23836605312d2827056c451aa06
BilBenAch <belalbenmousa@gmail.com>
2021-02-02 22:39:02 +0100

    hola mon

--- src/main.java
+++ src/main.java
@@ -1,17 +1,5 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("que tal");
-        System.out.println("primer proyecto");
-        System.out.println("hoasdasasasd");
-        System.out.println("segundo proyecto");
-        System.out.println("sada");
-        System.out.println("hola");
-        System.out.println("asdasas");
-        System.out.println("asaasasd");
-
-        System.out.println("aasasdasdadsasdadsdas");
-        System.out.println("asdadasasdasdasasdasd");
-
     }
 }
