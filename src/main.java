a8768a27654a7e21a0168069e1a5a5a7f65f21fa
BilBenAch <belalbenmousa@gmail.com>
2021-02-02 22:25:33 +0100

    primer proyecto

--- src/main.java
+++ src/main.java
@@ -2,6 +2,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("que tal");
+
+
         System.out.println("primer proyecto");
     }
 }
