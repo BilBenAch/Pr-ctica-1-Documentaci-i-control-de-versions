0a5463ba5b6d633128f88ae39bb68342df56694b
BilBenAch <belalbenmousa@gmail.com>
2021-02-02 22:56:13 +0100

    first commit

--- src/main.java
+++ src/main.java
@@ -1,9 +1,5 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("hola lluna");
-        System.out.println("repositorio 2");
-        System.out.println("frase 1");
-        System.out.println("repositorio 1");
     }
 }
