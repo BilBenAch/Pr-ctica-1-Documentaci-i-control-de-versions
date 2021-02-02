75ccb740bbffd9978c750ad8bfe47435cf5ed9f6
BilBenAch <belalbenmousa@gmail.com>
2021-02-02 22:29:03 +0100

    update

--- src/main.java
+++ src/main.java
@@ -2,8 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("que tal");
-
-
         System.out.println("primer proyecto");
+        System.out.println("hoasdasasasd");
     }
 }
