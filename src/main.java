96f62c80d5b0ce5990e4fdec909fe4a57a4bbf7e
BilBenAch <belalbenmousa@gmail.com>
2021-02-02 22:20:57 +0100

    segundo proyecto creado

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("que tal");
+        System.out.println("segundo proyecto");
     }
 }
